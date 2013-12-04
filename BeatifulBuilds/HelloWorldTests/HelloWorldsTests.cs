f7a3facde9ee8163b0ec1a3f27bd12bb4a0412dd
cvind <cv@ihpostal.dk>
2013-12-04 14:10:08 +0100

    Added HelloWorld solution with NuGet package and unittest

---
+++ BeatifulBuilds/HelloWorldTests/HelloWorldsTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+namespace HelloWorldTests
+{
+    [TestFixture]
+    class HelloWorldsTests
+    {
+        [Test]
+        void FailingTest()
+        {
+            Assert.AreEqual(true, false);
+        }
+
+        [Test]
+        void SucceedingTest()
+        {
+            Assert.AreEqual(true, true);
+        }
+
+        [Test]
+        void InconclusiveTest()
+        {
+            Assert.Inconclusive();
+        }
+    }
+}
